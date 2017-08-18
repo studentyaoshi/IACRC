93f9966756320326d3c443f458a53bcbb2944fb3
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-08-18 21:55:21 +0800

    change_cov_py1

--- pipeline/getpairs.py
+++ pipeline/getpairs.py
@@ -2,7 +2,7 @@ import sys
 a=open(sys.argv[1],'rt')
 for i in a:
 	snp1=i.rstrip('\n')
-	b=open(sys.argv[2],'wt')
+	b=open(sys.argv[2],'rt')
 	for w in b:
 		snp2=w.rstrip('\n')
 		print snp1+'\t'+snp2
