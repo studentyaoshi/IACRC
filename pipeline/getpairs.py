5f5d3f3a63464a565241c32b9681033695c8396d
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-08-18 21:46:00 +0800

    add_cov_py

---
+++ pipeline/getpairs.py
@@ -0,0 +1,10 @@
+import sys
+a=open(sys.argv[1],'rt')
+for i in a:
+	snp1=i.rstrip('\n')
+	b=open(sys.argv[2],'wt')
+	for w in b:
+		snp2=w.rstrip('\n')
+		print snp1+'\t'+snp2
+	b.close()
+a.close()
