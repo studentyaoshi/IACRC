a0b160aff6b1cd1f9db0b5ca3d44460928d0aba9
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-08-19 15:26:00 +0800

    edit change_covresult.py

--- pipeline/change_covresult.py
+++ pipeline/change_covresult.py
@@ -1,7 +1,7 @@
 import sys
 a=open(sys.argv[1],'rt')
 b=open(sys.argv[2],'wt')
-b.write('SNP1\tSNP2\tOR_INT\tP\n')
+b.write('SNP1\tSNP2\tP\tOR_INT\n')
 for i in a:
 	b.write(i.replace('_','\t').replace(' ','\t'))
 a.close()
