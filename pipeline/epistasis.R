7d0b319a9a81e956ec330892ffad1f3f5ee30217
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-10-18 20:01:42 +0800

    change epistasis.R

--- pipeline/epistasis.R
+++ pipeline/epistasis.R
@@ -7,11 +7,15 @@ snp2 <- snps[[1]][2]
 input <- read.table(filename1,header=T)
 if(length(unique(input$PHENO))==2){
 	am.data = glm(formula=paste('PHENO~snp1+snp2+snp1*snp2+',args[4],sep=""),data=input,family=binomial)
+	p<-coef(summary(am.data))[,4]
+	write.table(p,args[3],col.names=F,quote=F)
+	or<-exp(coef(am.data))
+	write.table(or,args[3],append=TRUE,col.names=F,quote=F)
 }else{
 	am.data = glm(formula=paste('PHENO~snp1+snp2+snp1*snp2+',args[4],sep=""),data=input,family=gaussian)
+	p<-coef(summary(am.data))[,4]
+	write.table(p,args[3],col.names=F,quote=F)
+	or<-coef(am.data)
+	write.table(or,args[3],append=TRUE,col.names=F,quote=F)
 }
 #am.data = glm(formula=PHENOTYPE~ snp1 + snp2 + snp1 * snp2,data=input,family=binomial)
-p<-coef(summary(am.data))[,4]
-write.table(p,args[3],col.names=F,quote=F)
-or<-exp(coef(am.data))
-write.table(or,args[3],append=TRUE,col.names=F,quote=F)
