7771cf2eaa6962c94fa72a15cfec155b51a09a74
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-10-18 20:12:33 +0800

    change cat_snppairs.sh

---
+++ pipeline/cat_snppairs.sh
@@ -0,0 +1,14 @@
+Numbers=(ZERO a b c d e f g h i j k l m n o p q r s t)
+
+thread_number=`grep Thread ../original/files|awk -F ' ' '{print$2}'`
+
+for ((i=1; i<=thread_number; i++))
+do cat ../result/${1}/${1}.pairs.${Numbers[$i]} ../result/${1}/${1}.pairs.hic.${Numbers[$i]} >> ../result/${1}/${1}.pairs
+done
+
+sort ../result/${1}/${1}.pairs|uniq > ../result/${1}/${1}.pairs.uniq
+
+row_number=`cat ../result/${1}/${1}.pairs.uniq|wc -l`
+let filenumber=${row_number}/${thread_number}+1
+split -l ${filenumber} ../result/${1}/${1}.pairs.uniq ../result/${1}/
+
