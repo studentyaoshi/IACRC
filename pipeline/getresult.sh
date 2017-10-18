a771e69b3a62e18a2c020ddca670ec0e829c3ebf
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-10-18 20:19:10 +0800

    change getresult.sh

--- pipeline/getresult.sh
+++ pipeline/getresult.sh
@@ -1,3 +1,20 @@
+Numbers=(ZERO a b c d e f g h i j k l m n o p q r s t)
+
+thread_number=`grep Thread ../original/files|awk -F ' ' '{print$2}'`
+
+for ((i=1; i<=thread_number; i++))
+do cat ../result/${1}/${1}.all.result.${Numbers[$i]} >> ../result/${1}/${1}.all.result
+done
+
+python change_covresult.py ../result/${1}/${1}.all.result ../result/${1}/${1}.allepi.change
+rm ../result/${1}/${1}.all.result
+plink --noweb --bfile ${2} --r2 --ld-window-kb 1000 --ld-window 99999 --ld-window-r2 ${ld} --out ../result/ld/${1}
+python nold05.py ../result/ld/${1}.ld ../result/${1}/${1}.allepi.change ../result/${1}/${1}.allepi.change.nold05
+rm ../result/${1}/${1}.allepi.change
+sort -r ../result/${1}/${1}.allepi.change.nold05|uniq> ../result/${1}/${1}.allepi.uniq
+rm ../result/${1}/${1}.allepi.change.nold05
+mv ../result/${1}/${1}.allepi.uniq ../result/${1}/${1}.allepi
+
 cd ../result/${1}/result
 ld=`grep LD_threshold ../original/files|awk -F ' ' '{print$2}'`
 ls|grep -E "epi.qt$|epi.cc$"|while read line
