4fe81ce77d637fdc155231d2dd33271bcc5a0382
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-10-30 17:40:23 +0800

    3

--- pipeline/cat_snppairs.sh
+++ pipeline/cat_snppairs.sh
@@ -6,8 +6,10 @@ for ((i=1; i<=thread_number; i++))
 do cat ../result/${1}/${1}.pairs.${Numbers[$i]} ../result/${1}/${1}.pairs.hic.${Numbers[$i]} >> ../result/${1}/${1}.pairs
 done
 
-sort ../result/${1}/${1}.pairs|uniq > ../result/${1}/${1}.pairs.uniq
+python pairs_sort.py ../result/${1}/${1}.pairs ../result/${1}/${1}.pairs.sort
 
-row_number=`cat ../result/${1}/${1}.pairs.uniq|wc -l`
-let filenumber=${row_number}/${thread_number}+1
-split -l ${filenumber} ../result/${1}/${1}.pairs.uniq ../result/${1}/
+sort ../result/${1}/${1}.pairs.sort|uniq > ../result/${1}/${1}.pairs.uniq
+
+# row_number=`cat ../result/${1}/${1}.pairs.uniq|wc -l`
+# let filenumber=${row_number}/${thread_number}+1
+# split -l ${filenumber} ../result/${1}/${1}.pairs.uniq ../result/${1}/
