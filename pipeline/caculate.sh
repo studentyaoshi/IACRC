8e95dbd22b469edd0ee4568a8476c63b65d7a767
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-11-01 18:12:01 +0800

    change caculate.sh: add path

--- pipeline/caculate.sh
+++ pipeline/caculate.sh
@@ -1,6 +1,13 @@
 Numbers=(ZERO a b c d e f g h i j k l m n o p q r s t)
 thread_number=`grep Thread ../original/files|awk -F ' ' '{print$2}'`
 
+R=`grep R_path ../original/files|awk -F ' ' '{print$2}'`
+Rscript=${R}'script'
+python=`grep Python_path ../original/files|awk -F ' ' '{print$2}'`
+bedtools=`grep Bedtools_path ../original/files|awk -F ' ' '{print$2}'`
+plink=`grep Plink_path ../original/files|awk -F ' ' '{print$2}'`
+perl=`grep Perl_path ../original/files|awk -F ' ' '{print$2}'`
+
 echo Start caculating
 echo Please wait ...
 echo This step can be long, plz try smaller file first ...
@@ -8,11 +15,11 @@ echo This step can be long, plz try smaller file first ...
 for ((i=1; i<=thread_number; i++))
 do cat ../result/$1/a${Numbers[$i]}|while read line
 do 
-	plink -bfile ${2} --snps $line --recodeA --out ../result/${1}/cov/$line
+	$plink -bfile ${2} --snps $line --recodeA --out ../result/${1}/cov/$line
 	rm ../result/${1}/cov/${line}.log
-	python changeraw.py ${3} ../result/${1}/cov/${line}.raw ../result/${1}/cov/${line}.recode
+	$python changeraw.py ${3} ../result/${1}/cov/${line}.raw ../result/${1}/cov/${line}.recode
 	covs=`head -1 ${3}|cut -f 3-|sed 's/\t/+/g'`
-	Rscript epistasis.R ${line} ../result/${1}/cov/${line}.recode ../result/${1}/result/${line}.result $covs
+	$Rscript epistasis.R ${line} ../result/${1}/cov/${line}.recode ../result/${1}/result/${line}.result $covs
 	rm ../result/${1}/cov/${line}.raw ../result/${1}/cov/${line}.recode
 	snp1=`echo "$line"|awk -F, '{print$1}'`
 	snp2=`echo "$line"|awk -F, '{print$2}'`
