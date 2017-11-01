bfc25af1bb4d95e2ace03dfc2bd87907f390fbf1
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-11-01 18:02:02 +0800

    change cat_snppairs.sh

--- pipeline/cat_snppairs.sh
+++ pipeline/cat_snppairs.sh
@@ -1,12 +1,12 @@
 Numbers=(ZERO a b c d e f g h i j k l m n o p q r s t)
-
+python=`grep Python_path ../original/files|awk -F ' ' '{print$2}'`
 thread_number=`grep Thread ../original/files|awk -F ' ' '{print$2}'`
 
 for ((i=1; i<=thread_number; i++))
 do cat ../result/${1}/${1}.pairs.${Numbers[$i]} ../result/${1}/${1}.pairs.hic.${Numbers[$i]} >> ../result/${1}/${1}.pairs
 done
 
-python pairs_sort.py ../result/${1}/${1}.pairs ../result/${1}/${1}.pairs.sort
+$python pairs_sort.py ../result/${1}/${1}.pairs ../result/${1}/${1}.pairs.sort
 
 sort ../result/${1}/${1}.pairs.sort|uniq > ../result/${1}/${1}.pairs.uniq
 
