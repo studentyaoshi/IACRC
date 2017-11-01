76ad1fe387f66b3201b7b02e633e54e951d3d9f0
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-11-01 14:07:06 +0800

    change purning.py 165

--- pipeline/purning.py
+++ pipeline/purning.py
@@ -43,11 +43,17 @@ for i in a:
 	ld[snp2_snp1]='1'
 a.close()
 a=open(sys.argv[11],'rt')
-b=open(sys.argv[12],'wt')
+exon={}
+for i in a:
+	snp=i.rstrip('\n')
+	exon[snp]='1'
+a.close()
+a=open(sys.argv[12],'rt')
+b=open(sys.argv[13],'wt')
 for i in a:
 	snp1,snp2=i.strip().split(',')
 	snp1_snp2=snp1+'_'+snp2
-	if cr.get(snp1)=='1' and cr.get(snp2)=='1' and maf.get(snp1)='1' and maf.get(snp2)='1' and hardy.get(snp1)='1' and hardy.get(snp2)='1' and ld.get(snp1_snp2)!='1':
+	if cr.get(snp1)=='1' and cr.get(snp2)=='1' and maf.get(snp1)='1' and maf.get(snp2)='1' and hardy.get(snp1)='1' and hardy.get(snp2)='1' and ld.get(snp1_snp2)!='1' and exon.get(snp1)!='1' and exon.get(snp2)!='1':
 		b.write(i)
 a.close()
 b.close()
