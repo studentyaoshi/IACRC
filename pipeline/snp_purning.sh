b62ab595d172c8c7899545c00de6bf2547dd2568
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-11-01 14:08:45 +0800

    change snp_purning.sh 166

--- pipeline/snp_purning.sh
+++ pipeline/snp_purning.sh
@@ -12,15 +12,15 @@ rm ../original/anno.xaa ../original/anno.xab ../original/anno.xac ../original/an
 perl table_annovar.pl ${2}.anno.tem ../original/ -buildver hg19 -out ${2}.ex.out -protocol wgEncodeGencodeBasicV19 -operation g
 python nosamegene.py ${2}.ex.out.wgEncodeGencodeBasicV19.variant_function ../result/${1}/${1}.pairs.uniq ../result/${1}/${1}.pairs.nos
 grep exonic ${2}.ex.out.wgEncodeGencodeBasicV19.variant_function |cut -f 8 > ${2}.exon.snp
-
 plink --bfile ${2} --freq --missing --hardy --out ${2} 
 grep ALL ${2}.hwe >${2}.hardy
 plink --bfile ${2} --r2 --ld-window-kb 1000 --ld-windwo 99999 --ld-window-r2 ${MAF} --out ${2}
-python purning.py ${2}.imiss ${2}.keep.fam ${IMR} ${2}.lmiss ${CR} ${2}.frq ${MAF} ${2}.hardy ${HWE} ${2}.ld ../result/${1}/${1}.pairs.uniq ../result/${1}/${1}.pairs.purning
+
+python purning.py ${2}.imiss ${2}.keep.fam ${IMR} ${2}.lmiss ${CR} ${2}.frq ${MAF} ${2}.hardy ${HWE} ${2}.ld ${2}.exon.snp ../result/${1}/${1}.pairs.nos ../result/${1}/${1}.pairs.purning
 
 plink --bfile ${2} --keep ${2}.keep.fam --make-bed --out ${2}.keep
 
-rm ${2}.log ${2}.frq ${2}.hwe ${2}.lmiss ${2}.imiss ${2}.hh ${2}.exon.snp ${2}.keep.fam ${2}.hardy
+rm ${2}.log ${2}.frq ${2}.hwe ${2}.lmiss ${2}.imiss ${2}.hh ${2}.exon.snp ${2}.keep.fam ${2}.hardy ${2}.exon.snp
 rm ${2}.anno.tem ${2}.ex.out.wgEncodeGencodeBasicV19.exonic_variant_function ${2}.ex.out.wgEncodeGencodeBasicV19.variant_function ${2}.ex.out.wgEncodeGencodeBasicV19.log ${2}.ex.out.hg19_multianno.txt
 
 row_number=`cat ../result/${1}/${1}.pairs.purning|wc -l`
