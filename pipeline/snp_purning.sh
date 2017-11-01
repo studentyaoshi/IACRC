63bb3e9affb7ce8f3e706e5af476d8d46f093195
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-11-01 11:41:58 +0800

    add snp_purning

--- pipeline/snp_purning.sh
+++ pipeline/snp_purning.sh
@@ -6,7 +6,6 @@ LD=`grep LD_threshold ../original/files|awk -F ' ' '{print$2}'`
 Numbers=(ZERO a b c d e f g h i j k l m n o p q r s t)
 thread_number=`grep Thread ../original/files|awk -F ' ' '{print$2}'`
 
-cut -f 2 ${2}.bim > ${2}.snp
 python snp_annotation.py ${2}.bim ${2}.anno.tem
 cat ../original/anno.xaa ../original/anno.xab ../original/anno.xac ../original/anno.xad ../original/anno.xae ../original/anno.xaf > ../original/hg19_wgEncodeGencodeBasicV19Mrna.fa
 rm ../original/anno.xaa ../original/anno.xab ../original/anno.xac ../original/anno.xad ../original/anno.xae ../original/anno.xaf
@@ -14,10 +13,13 @@ perl table_annovar.pl ${2}.anno.tem ../original/ -buildver hg19 -out ${2}.ex.out
 python nosamegene.py ${2}.ex.out.wgEncodeGencodeBasicV19.variant_function ../result/${1}/${1}.pairs.uniq ../result/${1}/${1}.pairs.nos
 grep exonic ${2}.ex.out.wgEncodeGencodeBasicV19.variant_function |cut -f 8 > ${2}.exon.snp
 
-plink --bfile ${2} --missing --hardy --out ${2}
-#python purning.py $${{2}.hwe ${2}.lmiss ${2}.imiss ${2}.hardy
+plink --bfile ${2} --freq --missing --hardy --out ${2} 
+plink --bfile ${2} --r2 --ld-window-kb 1000 --ld-windwo 99999 --ld-window-r2 ${MAF} --out ${2}
+python purning.py ${2}.imiss ${2}.keep.fam ${IMR} ${2}.lmiss ${CR} ${2}.frq ${MAF} 
 
-rm ${2}.log ${2}.hwe ${2}.lmiss ${2}.imiss ${2}.hh ${2}.hardy
+plink --bfile ${2} --keep ${2}.keep.fam --make-bed --out ${2}.keep
+
+rm ${2}.log ${2}.frq ${2}.hwe ${2}.lmiss ${2}.imiss ${2}.hh ${2}.exon.snp ${2}.keep.fam 
 rm ${2}.anno.tem ${2}.ex.out.wgEncodeGencodeBasicV19.exonic_variant_function ${2}.ex.out.wgEncodeGencodeBasicV19.variant_function ${2}.ex.out.wgEncodeGencodeBasicV19.log ${2}.ex.out.hg19_multianno.txt
 
 row_number=`cat ../result/${1}/${1}.pairs.uniq|wc -l`
