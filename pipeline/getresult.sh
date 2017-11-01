de2a7578fda253c42a52abefa492da421723ee24
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-11-01 18:13:39 +0800

    change getresult.sh: add path

--- pipeline/getresult.sh
+++ pipeline/getresult.sh
@@ -2,13 +2,13 @@ Numbers=(ZERO a b c d e f g h i j k l m n o p q r s t)
 
 thread_number=`grep Thread ../original/files|awk -F ' ' '{print$2}'`
 ld=`grep LD_threshold ../original/files|awk -F ' ' '{print$2}'`
-
+python=`grep Python_path ../original/files|awk -F ' ' '{print$2}'`
 
 for ((i=1; i<=thread_number; i++))
 do cat ../result/${1}/${1}.all.result.${Numbers[$i]} >> ../result/${1}/${1}.all.result
 done
 
-python change_covresult.py ../result/${1}/${1}.all.result ../result/${1}/${1}.allepi.change
+$python change_covresult.py ../result/${1}/${1}.all.result ../result/${1}/${1}.allepi.change
 sort -r ../result/${1}/${1}.allepi.change|uniq> ../result/${1}/${1}.allepi.uniq
 rm ../result/${1}/${1}.all.result ../result/${1}/${1}.allepi.change
 mv ../result/${1}/${1}.allepi.uniq ../result/${1}/${1}.allepi
