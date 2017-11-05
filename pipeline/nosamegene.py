76b3447f99ab0800248ea1f264e71661b186ca1f
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-11-05 22:30:22 +0800

    change nosamegene

--- pipeline/nosamegene.py
+++ pipeline/nosamegene.py
@@ -15,9 +15,9 @@ b=open(sys.argv[3],'wt')
 a.readline()
 for i in a:
 	snp1,snp2=i.strip().split(',')
-	if dic.get(snp1)==dic.get(snp2) and dic.get(snp1)!='1':
-		n=0
-	elif snp1[0]=='r' and snp2[0]=='r' and snp1!=snp2:
+#	if dic.get(snp1)==dic.get(snp2) and dic.get(snp1)!='1':
+#		n=0
+	if snp1[0]=='r' and snp2[0]=='r' and snp1!=snp2:
 		b.write(i)
 a.close()
 b.close()
