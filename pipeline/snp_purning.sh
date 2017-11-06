f3badbc6f06b27adc338b52a48c6cb3cfb465374
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-11-06 14:15:31 +0800

    change snp_purning.sh: have no exon problem

--- pipeline/snp_purning.sh
+++ pipeline/snp_purning.sh
@@ -10,25 +10,14 @@ R=`grep R_path ../original/files|awk -F ' ' '{print$2}'`
 python=`grep Python_path ../original/files|awk -F ' ' '{print$2}'`
 bedtools=`grep Bedtools_path ../original/files|awk -F ' ' '{print$2}'`
 plink=`grep Plink_path ../original/files|awk -F ' ' '{print$2}'`
-perl=`grep Perl_path ../original/files|awk -F ' ' '{print$2}'`
-annovar=`grep Annovar_path ../original/files|awk -F ' ' '{print$2}'`
 
-$python snp_annotation.py ${2}.bim ${2}.anno.tem
-cat ../original/anno.xaa ../original/anno.xab ../original/anno.xac ../original/anno.xad ../original/anno.xae ../original/anno.xaf > ../original/hg19_wgEncodeGencodeBasicV19Mrna.fa
-#rm ../original/anno.xaa ../original/anno.xab ../original/anno.xac ../original/anno.xad ../original/anno.xae ../original/anno.xaf
-$perl ${annovar}/table_annovar.pl ${2}.anno.tem ../original/ -buildver hg19 -out ${2}.ex.out -protocol wgEncodeGencodeBasicV19 -operation g
-$python nosamegene.py ${2}.ex.out.wgEncodeGencodeBasicV19.variant_function ../result/${1}/${1}.pairs.uniq ../result/${1}/${1}.pairs.nos
-grep exonic ${2}.ex.out.wgEncodeGencodeBasicV19.variant_function |cut -f 8 > ${2}.exon.snp
+$python nosamegene.py ../result/${1}/${1}.pairs.uniq ../result/${1}/${1}.pairs.nos
 $plink --bfile ${2} --freq --missing --hardy --out ${2} 
 grep ALL ${2}.hwe >${2}.hardy
 $plink --bfile ${2} --r2 --ld-window-kb 1000 --ld-window 99999 --ld-window-r2 ${LD} --out ${2}
-
-$python purning.py ${2}.imiss ${2}.keep.fam ${IMR} ${2}.lmiss ${CR} ${2}.frq ${MAF} ${2}.hardy ${HWE} ${2}.ld ${2}.exon.snp ../result/${1}/${1}.pairs.nos ../result/${1}/${1}.pairs.purning
-
+$python purning.py ${2}.imiss ${2}.keep.fam ${IMR} ${2}.lmiss ${CR} ${2}.frq ${MAF} ${2}.hardy ${HWE} ${2}.ld ../result/${1}/${1}.pairs.nos ../result/${1}/${1}.pairs.purning
 $plink --bfile ${2} --keep ${2}.keep.fam --make-bed --out ${2}.keep
-
-rm ${2}.log ${2}.frq ${2}.hwe ${2}.lmiss ${2}.imiss ${2}.hh ${2}.exon.snp ${2}.keep.fam ${2}.hardy ${2}.exon.snp
-rm ${2}.anno.tem ${2}.ex.out.wgEncodeGencodeBasicV19.exonic_variant_function ${2}.ex.out.wgEncodeGencodeBasicV19.variant_function ${2}.ex.out.wgEncodeGencodeBasicV19.log ${2}.ex.out.hg19_multianno.txt
+rm ${2}.log ${2}.frq ${2}.hwe ${2}.lmiss ${2}.imiss ${2}.hh ${2}.keep.fam ${2}.hardy
 
 row_number=`cat ../result/${1}/${1}.pairs.purning|wc -l`
 let filenumber=${row_number}/${thread_number}+1
