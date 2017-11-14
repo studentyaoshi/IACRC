99efd582a584acd07c04f24ed8c2b3f8fa2db0b2
ys <studentyaoshi@stu.xjtu.edu.cn>
2017-11-14 10:31:19 +0800

    change snp_purning.sh

--- pipeline/snp_purning.sh
+++ pipeline/snp_purning.sh
@@ -17,7 +17,7 @@ grep ALL ${2}.hwe >${2}.hardy
 $plink --bfile ${2} --r2 --ld-window-kb 1000 --ld-window 99999 --ld-window-r2 ${LD} --out ${2}
 $python purning.py ${2}.imiss ${2}.keep.fam ${IMR} ${2}.lmiss ${CR} ${2}.frq ${MAF} ${2}.hardy ${HWE} ${2}.ld ../result/${1}/${1}.pairs.nos ../result/${1}/${1}.pairs.purning
 $plink --bfile ${2} --keep ${2}.keep.fam --make-bed --out ${2}.keep
-rm ${2}.log ${2}.frq ${2}.hwe ${2}.lmiss ${2}.imiss ${2}.hh ${2}.keep.fam ${2}.hardy
+rm ${2}.log ${2}.frq ${2}.hwe ${2}.lmiss ${2}.imiss ${2}.hh ${2}.hardy
 
 row_number=`cat ../result/${1}/${1}.pairs.purning|wc -l`
 let filenumber=${row_number}/${thread_number}+1
